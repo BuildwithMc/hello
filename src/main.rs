bc1c6a3421b12e7079b03650d57d18f4a518e879
Anosike Mmerichukwu <Mmerichukwu.anosike.242580@unn.edu.ng>
2024-04-08 07:56:45 +0100

    hello world task done

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+ fn main() {
+    let x = 42;
+    let y = 550;
+
+    println!("x + y = {}  ", x +y);
+    println!("Hello world");
+    println!("Its BuildwithMc here, thank you secret network for this opportunity");
+}
